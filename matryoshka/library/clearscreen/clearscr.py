6e7a0231141c39bcf2ef63c6ba55c002bdc54a94
Mohammad Drikvand <mohammaddrikvand217@gmail.com>
2023-02-09 01:15:02 +0330

    Update clearscr

--- matryoshka/library/clearscreen/clearscr.py
+++ matryoshka/library/clearscreen/clearscr.py
@@ -3,7 +3,7 @@ import os
 #::::: clearScreen :::::
 def clearScr():
     """A Function To Clean Up The Command Prompt or Terminal."""
-    if os.name == "nt":
+    if (os.name == "nt"):
         os.system("cls")
     else:
         os.system("clear")
