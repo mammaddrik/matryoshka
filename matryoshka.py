d5bac2974b33947221df4e187270d24f02a9a890
Mohammad Drikvand <mohammaddrikvand217@gmail.com>
2023-02-10 05:25:20 +0330

    Update matryoshka
    Update matryoshka

--- matryoshka.py
+++ matryoshka.py
@@ -48,13 +48,14 @@ from matryoshka.library.slowprint.slowprint import slowprint
 from matryoshka.library.banner.banner import Banner
 
 #::::: Default Library :::::
+import webbrowser
 import sys
 import time
 import os
 
 #::::: Again :::::
 def again():
-    """A Function To Ask The User To Restart The Program."""
+    "A Function To Ask The User To Restart The Program."
     matryoshka_again = input(Color.BCyan+"""\n    Do You Want To Continue?"""+Color.End+"""\n\t┌───(matryoshka)─[~/again]─[Y/n]
 \t└─"""+color_banner[0]+"""$ """+Color.End)
     if (matryoshka_again.upper() == "Y" or matryoshka_again == ""):
@@ -72,7 +73,7 @@ def again():
 
 #::::: matryoshka :::::
 def matryoshka():
-    """The Main Function Of Matryoshka, The First Page Of The Program."""
+    "The Main Function Of Matryoshka, The First Page Of The Program."
     clearScr()
     time.sleep(0.4)
     print(Banner.matryoshka_banner)
@@ -295,86 +296,482 @@ def matryoshka():
             again()
             
         matryoshka()
-    
+        
+    #::::: Basic2 :::::
     elif (choice == "02" or choice == "2"):
-        pass
-
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.basic2_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/basic2]
+└─"""+color_banner[2]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()               
+    #::::: Basic3 :::::
     elif (choice == "03" or choice == "3"):
-        pass
-
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.basic3_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/basic3]
+└─"""+color_banner[3]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
+    #::::: Array :::::
     elif (choice == "04" or choice == "4"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.array_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/array]
+└─"""+color_banner[4]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
 
+    #::::: Conditional Statement :::::
     elif (choice == "05" or choice == "5"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.conditionalstatement_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Conditional Statement]
+└─"""+color_banner[5]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
 
+    #::::: Pattern :::::
     elif (choice == "06" or choice == "6"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.pattern_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Pattern]
+└─"""+color_banner[0]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
 
+    #::::: Function :::::
     elif (choice == "07" or choice == "7"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.function_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Function]
+└─"""+color_banner[1]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()        
 
+    #::::: Data & Time :::::
     elif (choice == "08" or choice == "8"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.datatime_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Data & Time]
+└─"""+color_banner[2]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
 
+    #::::: Maths :::::
     elif (choice == "09" or choice == "9"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.maths_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Maths]
+└─"""+color_banner[3]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()          
 
+    #::::: List :::::
     elif (choice == "10"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.list_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/List]
+└─"""+color_banner[4]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()        
 
+    #::::: String :::::
     elif (choice == "11"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.string_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/String]
+└─"""+color_banner[5]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
 
+    #::::: Dictionary :::::
     elif (choice == "12"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.dictionary_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Dictionary]
+└─"""+color_banner[0]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
 
+    #::::: Set :::::
     elif (choice == "13"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.set_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Set]
+└─"""+color_banner[1]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
 
+    #::::: Recurion :::::
     elif (choice == "14"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.recurion_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Recurion]
+└─"""+color_banner[2]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()            
 
+    #::::: Tuple :::::
     elif (choice == "15"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.tuple_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Tuple]
+└─"""+color_banner[3]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()          
 
+    #::::: File :::::
     elif (choice == "16"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.file_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/File]
+└─"""+color_banner[4]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
 
+    #::::: Oops :::::
     elif (choice == "17"):
-        pass
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.oops_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Oops]
+└─"""+color_banner[5]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
+        
 
+    #::::: Search & Sort :::::
     elif (choice == "18"):
-        pass
-    
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.searchsort_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Search & Sort]
+└─"""+color_banner[0]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()        
+
+    #::::: Sqlite :::::    
     elif (choice == "19"):
-        pass    
-    
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.sqlite_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Sqlite]
+└─"""+color_banner[1]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()             
+
+    #::::: Json :::::    
     elif (choice == "20"):
-        pass    
-    
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.json_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Json]
+└─"""+color_banner[2]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()  
+        
+    #::::: Collection :::::    
     elif (choice == "21"):
-        pass    
-    
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.collection_banner) 
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Collection]
+└─"""+color_banner[3]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()             
+
+    #::::: Lambda :::::    
     elif (choice == "22"):
-        pass    
-    
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.lambda_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Lambda]
+└─"""+color_banner[4]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()   
+
+    #::::: Os :::::    
     elif (choice == "23"):
-        pass    
-    
+        clearScr()
+        time.sleep(0.4) 
+        print(Banner.os_banner)  
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Os]
+└─"""+color_banner[5]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()   
+        
+    #::::: Heap Queue :::::    
     elif (choice == "24"):
-        pass    
-    
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.heapqueue_banner)   
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Heap Queue]
+└─"""+color_banner[0]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka() 
+                          
+    #::::: Re :::::    
     elif (choice == "25"):
-        pass    
-    
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.re_banner)    
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Re]
+└─"""+color_banner[1]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka() 
+        
+    #::::: Data Structure :::::    
     elif (choice == "26"):
-        pass    
-    
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.datastructure_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Data Structure]
+└─"""+color_banner[2]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka() 
+        
+    #::::: Csv :::::    
+    elif (choice == "27"):
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.csv_banner)    
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Csv]
+└─"""+color_banner[3]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka() 
+        
+    #::::: Linked List :::::        
+    elif (choice == "28"):
+        clearScr()
+        time.sleep(0.4)
+        print(Banner.linkedlist_banner)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Linked List]
+└─"""+color_banner[4]+"""$ """+Color.End)
+        if (select == "01" or select == "1"):
+            pass
+        elif (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()         
+    #::::: Github :::::
     elif (choice == "00" or choice == "0"):
         clearScr()
+        time.sleep(0.4)
         print(Banner.github_banner)
-    
+        url = "https://github.com/mammaddrik"
+        webbrowser.open(url)
+        select = input("""
+┌───(matryoshka)─[~/matryoshka/Github]
+└─"""+color_banner[5]+"""$ """+Color.End)
+        if (select == "00" or select == "0"):
+            matryoshka()
+        else:
+            again()
+            
+        matryoshka()
+                
+    #::::: Exit :::::
     elif (choice == "99"):
         print("\nGoodBye :)")
         time.sleep(0.4)
@@ -382,6 +779,7 @@ def matryoshka():
         sys.exit()
            
     again()
+    
 try:
     matryoshka()
     again()
